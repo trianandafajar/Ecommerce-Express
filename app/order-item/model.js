65d2310d40a1b4a5e1a257dcdb48adba2d4684a7
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-28 11:29:17 +0700

    Update Order Item Modal

--- app/order-item/model.js
+++ app/order-item/model.js
@@ -2,33 +2,32 @@ const mongoose = require('mongoose');
 const { model, Schema } = mongoose;
 
 const orderItemSchema = Schema({
-
   name: {
-    type: String, 
-    minlength: [5, 'Panjang nama makanan minimal 50 karakter'],
-    required: [true, 'name must be filled']
+    type: String,
+    minlength: [5, 'Panjang nama makanan minimal 5 karakter'],
+    required: [true, 'Nama produk harus diisi'],
   },
 
   price: {
-    type: Number, 
-    required: [true, 'Harga item harus diisi']
-  }, 
+    type: Number,
+    required: [true, 'Harga produk harus diisi'],
+  },
 
   qty: {
-    type: Number, 
+    type: Number,
     required: [true, 'Kuantitas harus diisi'],
-    min: [1, 'Kuantitas minimal 1']
+    min: [1, 'Kuantitas minimal 1'],
   },
 
   product: {
-    type: Schema.Types.ObjectId, 
-    ref: 'Product'
-  }, 
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+  },
 
   order: {
-    type: Schema.Types.ObjectId, 
-    ref: 'Order'
-  }
+    type: Schema.Types.ObjectId,
+    ref: 'Order',
+  },
 });
 
 module.exports = model('OrderItem', orderItemSchema);
