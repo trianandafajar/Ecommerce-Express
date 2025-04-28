a8f8dff75cf831c01ed44165b8220e478c3097c1
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-28 11:28:34 +0700

    Update Policy

--- app/policy/index.js
+++ app/policy/index.js
@@ -1,68 +1,42 @@
-const { AbilityBuilder, Ability } = require("@casl/ability");
+const { AbilityBuilder, Ability } = require('@casl/ability');
 
 const policies = {
 
-   guest(user, {can}){
-     can('read', 'Product');
+   guest(user, { can }) {
+     can('read', 'Product'); // Guest hanya bisa melihat produk
    },
 
-   user(user, {can}){
-     // membaca daftar `Order`
-     can('view', 'Order');
-
-     // membuat `Order` 
-     can('create', 'Order');
-
-     // membaca `Order` miliknya 
-     can('read', 'Order', {user_id: user._id});
-
-     // mengupdate data dirinya sendiri (`User`)
-     can('update', 'User', {_id: user._id});
-
-     // membaca `Cart` miliknya
-     can('read', 'Cart', {user_id: user._id});
-
-     // mengupdate `Cart` miliknya
-     can('update', 'Cart', {user_id: user.id});
-
-     // melihat daftar `DeliveryAddress` 
-     can('view', 'DeliveryAddress');
-
-     // membuat `DeliveryAddress`
-     can('create', 'DeliveryAddress', {user_id: user._id});
-
-     // membaca `DeliveryAddress` miliknya 
-     can('read', 'DeliveryAddress', {user_id: user._id});
-
-     // mengupdate `DeliveryAddress` miliknya 
-     can('update', 'DeliveryAddress', {user_id: user._id});
-
-     // menghapus `DeliveryAddress` miliknya 
-     can('delete', 'DeliveryAddress', {user_id: user._id});
-
-     // membaca `Invoice` miliknya
-     can('read', 'Invoice', {user_id: user._id});
+   user(user, { can }) {
+     can('view', 'Order'); // User bisa melihat pesanan
+     can('create', 'Order'); // User bisa membuat pesanan
+     can('read', 'Order', { user_id: user._id }); // User hanya bisa melihat pesanan mereka sendiri
+     can('update', 'User', { _id: user._id }); // User bisa mengupdate data dirinya sendiri
+     can('read', 'Cart', { user_id: user._id }); // User bisa membaca keranjang miliknya
+     can('update', 'Cart', { user_id: user._id }); // User bisa mengupdate keranjang miliknya
+     can('view', 'DeliveryAddress'); // User bisa melihat alamat pengiriman
+     can('create', 'DeliveryAddress', { user_id: user._id }); // User bisa menambah alamat pengiriman
+     can('read', 'DeliveryAddress', { user_id: user._id }); // User bisa membaca alamat pengiriman miliknya
+     can('update', 'DeliveryAddress', { user_id: user._id }); // User bisa mengupdate alamat pengiriman miliknya
+     can('delete', 'DeliveryAddress', { user_id: user._id }); // User bisa menghapus alamat pengiriman miliknya
+     can('read', 'Invoice', { user_id: user._id }); // User bisa membaca invoice miliknya
    },
 
-   admin(user, {can}){
-     can('manage', 'all');
+   admin(user, { can }) {
+     can('manage', 'all'); // Admin bisa mengelola semua resource
    }
 }
 
-
-function policyFor(user){
-
+function policyFor(user) {
   let builder = new AbilityBuilder();
 
-  if(user && typeof policies[user.role] === 'function'){
+  if (user && typeof policies[user.role] === 'function') {
     policies[user.role](user, builder);
   } else {
     policies['guest'](user, builder);
-  } 
+  }
 
   return new Ability(builder.rules);
 }
-
 
 module.exports = {
   policyFor
